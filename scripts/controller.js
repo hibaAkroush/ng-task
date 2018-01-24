2fce67829dc947c089d7028abbf88c17434ecb96
Hiba Akroush <HAkroush@optimizasolutions.com>
2018-01-24 15:28:42 +0200

    delete

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,10 +2,27 @@ myApp.controller('myCtrl',function ($scope, $http) {
 	$scope.employee = {
         id: null
      };
+     $scope.managment = {
+        id: null
+     };
 	$scope.deleteEntity = function () {
-		index = $scope.employee.id
+		for (var i = 0; i < this.emps.length; i++) {
+			if (this.emps[i].id === $scope.employee.id.id) {
+				index = i
+			}
+		}
 		if(confirm("are you sure?"))
 		$scope.emps.splice(index,1)
+	}
+	$scope.deleteManagment = function () {
+		for (var i = 0; i < this.deps.length; i++) {
+			if (this.deps[i].id === $scope.managment.id.id) {
+				index = i
+			}
+		}
+		if(confirm("are you sure?"))
+		$scope.deps.splice(index,1)
+
 	}
 
 	$http.get('../data/dep.json')
@@ -15,7 +32,6 @@ myApp.controller('myCtrl',function ($scope, $http) {
 
 	$http.get('../data/emp.json')
 	.then(function(response) {
-		console.log(response.data)
     	$scope.emps = response.data
 	})
 })
